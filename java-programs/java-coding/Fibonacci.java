c2255c5b8d10130be67b00f489e462a5120abfd3
sushmamainampati <sushma26.m@gmail.com>
2025-07-04 18:03:56 +0530

    Fibonacci

---
+++ java-programs/java-coding/Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10; 
+        int a = 0;
+        int b = 1;
+        System.out.println("Fibonacci series up to " + n + " terms:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+    }
+}
